aa5e60cfe4029ee04be1ee0e325eb8b918d4cdbf
Vura .  Jyothi sri <122161160+vurajyothisri@users.noreply.github.com>
2023-06-08 12:24:08 +0000

    commit

---
+++ src/models/product.schema.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const ProductSchema=new mongoose.Schema({
+    name:{
+        type:String,
+    require:['true','please provide product name'] ,
+    trim:true,
+    maxLength:[120," product name should not be max than 120 chars"]   },
+price:{
+    type:Number,
+    require:['true','please provide product price'] ,
+    maxLength:[5," product name should not be max than 5 chars"]
+},
+description:{
+    type:String,
+},
+photos:[
+    {
+        secure_url:{
+            type:String,
+            required:true
+        }
+    },
+   
+],
+stock:{
+    type:Number,
+    default:0
+},
+sold:{
+    type:Number,
+    default:0,
+},
+collectionId:{
+    ref:"Collections"
+}
+},{timestamps:true})
+
+export default mongoose.model("Product",ProductSchema)
